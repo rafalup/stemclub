f894390bbd6852237241e7aa192d8cb75f6b5a7f
rafalup <arruda.rafa.ra@gmail.com>
2022-02-27 20:46:49 -0300

    Updates

---
+++ precache-manifest.c634a5feeb428ecf8b4a65095f6a34a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f98eb338141e524e34",
+    "url": "/stemclub/static/css/main.bf9c6068.chunk.css"
+  },
+  {
+    "revision": "c9f98eb338141e524e34",
+    "url": "/stemclub/static/js/main.c9f98eb3.chunk.js"
+  },
+  {
+    "revision": "f61546c6a5ec5f951c35",
+    "url": "/stemclub/static/css/1.ad1c7a2d.chunk.css"
+  },
+  {
+    "revision": "f61546c6a5ec5f951c35",
+    "url": "/stemclub/static/js/1.f61546c6.chunk.js"
+  },
+  {
+    "revision": "0262c8f112f2bf36b383",
+    "url": "/stemclub/static/js/runtime~main.0262c8f1.js"
+  },
+  {
+    "revision": "61e1229de043f02694f89f58ec172e21",
+    "url": "/stemclub/static/media/x.61e1229d.svg"
+  },
+  {
+    "revision": "272f75e041cc3a5b231325b1129d2011",
+    "url": "/stemclub/static/media/menu.272f75e0.svg"
+  },
+  {
+    "revision": "d7c45a2a2b2dc6dddbc73dd17901b41f",
+    "url": "/stemclub/static/media/logo.d7c45a2a.svg"
+  },
+  {
+    "revision": "574871b4277c5aab140dcd11f11ef7de",
+    "url": "/stemclub/index.html"
+  }
+];
